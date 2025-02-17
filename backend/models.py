b2ed2feb54cb568103e0bf9302cdba956de4198d
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-02-16 22:34:52 -0300

    fix error model user/post

--- backend/models.py
+++ backend/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)  # Ensure username is unique
     password = db.Column(db.String(200), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+    user_posts = db.relationship('Post', back_populates='user', overlaps="author,posts")
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,6 +15,5 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     author_username = db.Column(db.String(80), db.ForeignKey('user.username'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)  # Adding created_at
-
+    user = db.relationship('User', back_populates='user_posts', overlaps="author,posts")
     # Relationship with User
-    user = db.relationship('User', backref='user_posts')
